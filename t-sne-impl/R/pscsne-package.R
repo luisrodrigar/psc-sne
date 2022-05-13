d0b73f541955589edd9c4e2a905e290631175403
Luis Angel Rodriguez Garcia <luisrodrigar@MacBook-Pro-de-Luis.local>
2022-05-14 00:34:51 +0200

    Added two folders: R and tests

---
+++ t-sne-impl/R/pscsne-package.R
@@ -0,0 +1,15 @@
+
+
+#' @title \code{pscsne}: Uniformity Tests on the Circle, Sphere, and
+#' Hypersphere
+#'
+#' @description Implementation of the Polyspherical-Cauchy-SNE
+#'
+#' @author Eduardo García-Portugués and Luis Ángel Rodríguez García.
+#'
+#' @docType package
+#' @name pscsne-package
+#' @import graphics Rcpp stats
+#' @useDynLib pscsne
+#' @aliases pscsne pscsne-package
+NULL
