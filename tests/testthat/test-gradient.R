6e39c4cc28cacbcf2fcf15844a289a215451aa55
Luis Angel Rodriguez Garcia <luisrodrigar@MacBook-Pro-de-Luis.local>
2022-05-25 19:29:15 +0200

    Add test to check the gradient function

--- tests/testthat/test-gradient.R
+++ tests/testthat/test-gradient.R
@@ -85,7 +85,8 @@ q_ij_yi <- function(Y, yi, i, j, rho, d, diff_i) {
   return(qij / Z_yi(Y, rho, d, yi, diff_i))
 }
 
-kl_cost_i <- function(Y, P, rho, d, yi, ii) {
+kl_div_obj_func <- function(Y, P, rho, d, yi, ii) {
+  Y <- radial_projection(Y)
   n <- nrow(Y)
   total <- sum(sapply(1:n, function(i){
     sapply((1:n), function(j) {
@@ -100,24 +101,56 @@ kl_cost_i <- function(Y, P, rho, d, yi, ii) {
 }
 
 n <- 25
+p <- 3
 d <- 2
 r <- 5
+rho <- 0.5
 perplexity <- 15
 
 X <- gen_polysphere(n, d, r)
 rho_hat <- rho_optimize(X, perplexity)
-P <- high_dimension_p(X, rho_hat, d)
+P <- high_dimension(X, rho_hat)
 P <- symmetric_probs(P)
-
-rho <- 0.5
-Y <- r_unif_sphere(n, d+1)
-Q <- low_dimension_Q(Y, d, rho)
+Y <- r_unif_sphere(n, p)
 
 ii <- 1
 yi <- Y[ii,]
 
-test_that("Gradient Descent checking", {
-  expect_equal(jacobian(kl_cost_i, Y=Y, P=P, rho=rho, d=d, x=yi, i=ii), 
-               kl_cost_analytic(Y, ii, rho, d, P, Q), 
-               tolerance=1e-2, ignore_attr = TRUE)
+test_that("Checking value with gradient approximation", {
+  expect_equal(jacobian(kl_div_obj_func, Y=Y, P=P, rho=rho, d=d, x=yi, i=ii), 
+               kl_divergence_grad(Y, ii, rho, d, P), 
+               tolerance=1e-6, ignore_attr = TRUE)
 })
+
+test_that("Checking that the radial projetion is done", {
+  Y_non_in_the_sphere <- rmvnorm(n, c(1,2,3), diag(3))
+  Y_sphere <- radial_projection(Y_non_in_the_sphere)
+  expect_equal(kl_divergence_grad(Y_non_in_the_sphere, ii, rho, d, P),
+               kl_divergence_grad(Y_sphere, ii, rho, d, P))
+})
+
+test_that("Checking that i is valid", {
+  expect_error(kl_divergence_grad(Y, 120, rho, d, P))
+})
+
+test_that("Checking that d is valid", {
+  expect_error(kl_divergence_grad(Y, ii, rho, 0, P))
+})
+
+test_that("Checking that rho is valid", {
+  expect_error(kl_divergence_grad(Y, ii, 1, d, P))
+  expect_error(kl_divergence_grad(Y, ii, -1, d, P))
+})
+
+test_that("Checking that the number of observation is according with the number of rows in P", {
+  Y_wrong_rows <- r_unif_sphere(2, p)
+  expect_error(kl_divergence_grad(Y_wrong_rows, ii, rho, d, P))
+})
+
+test_that("Checking that the dimension of Y is according with the given d", {
+  Y_wrong_rows <- r_unif_sphere(n, 1)
+  expect_error(kl_divergence_grad(Y_wrong_rows, ii, rho, d, P))
+  Y_wrong_rows <- r_unif_sphere(n, 2)
+  expect_error(kl_divergence_grad(Y_wrong_rows, ii, rho, d, P))
+})
+
