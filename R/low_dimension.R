ed10a3cf2dc013858fb3cf7ef18b2ac4de44c048
Luis Angel Rodriguez Garcia <luisrodrigar@MacBook-Pro-de-Luis.local>
2022-06-21 22:23:09 +0200

    Added comments to low_dimension R file

--- R/low_dimension.R
+++ R/low_dimension.R
@@ -5,11 +5,44 @@
 
 library(lsa)
 
-low_dimension_Q <- function(Y, d, rho) {
-  cos_simil <- cosine(t(Y))
+#' Projection of the points onto the sphere of radius 1
+#'
+#' @param x matrix with the points in the sphere
+#' @return a matrix with the values of x projected onto the sphere of radius 1
+#' @examples
+#' radial_projection(x)
+radial_projection <- function(x) {
+  # For every observation, apply the formula of the radial projection
+  t(sapply(1:nrow(x), function(i) {
+    # x_i / |x_i|
+    x[i, ] / norm(x[i, ], type = "2")
+  }))
+}
+
+#' Calculate the low-dimension probabilities of a reduced matrix Y
+#' Using 
+#'
+#' @param Y matrix with the points in the sphere
+#' @param rho parameter between 0 and 1 (not included)
+#' @return a matrix with the values of x projected onto the sphere of radius 1
+#' @examples
+#' low_dimension_Q(Y, 0)
+#' low_dimension_Q(Y, 0.5)
+#' low_dimension_Q(Y, 0.9999)
+low_dimension_Q <- function(Y, rho) {
+  # Obtaining d, where S^d
+  d <- nrow(Y)-1
+  # Projecting the points onto de sphere, in case they are not
+  Z <- radial_projection(Y)
+  # Calculate the cosine similarities matrix of Z
+  cos_simil <- cosine(t(Z))
+  # Applying the Spherical Cauchy low-dimension joint function
   Q <- (1 + rho^2 - 2 * rho * cos_simil)^(-d)
+  # Set the elements of the diagonal to zero
   diag(Q) <- 0
+  # Calculate the total probability
   Qi <- sum(Q)
+  # Calculate the matrix probability of the joint probabilities 
   Q_ij <- Q / Qi
   return(Q_ij)
 }
