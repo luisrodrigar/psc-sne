12be97011a0132737a7dd6e9d69260b8806b808c
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2022-08-25 13:23:08 +0200

    Fixes gradient and adds small RNA example (#27)
    * Change norm function by sqrt(sum(x^2)) since it could cause some issues when dealing with matrices
    
    * Fix gradient
    
    * Add smallrna + jdf datasets
    
    * Rename
    
    * Small RNA addressing #6
    
    * Return tree + intrinsic distances
    
    * Add convergence diagnostics
    
    * Add the radial projection inmediately after calculating the gradiant computationally on unitary tests
    
    Co-authored-by: Luis Angel Rodriguez Garcia <luisrodrigar11@gmail.com>

---
+++ R/data.R
@@ -0,0 +1,46 @@
+
+
+#' @title Juan de Fuca currents
+#'
+#' @description TODO
+#'
+#' @docType data
+#' @format A data frame with TODO rows and TODO variables:
+#' \describe{
+#'   \item{TODO}{TODO}
+#'   \item{TODO}{TODO}
+#' }
+#' @details
+#' TODO
+#' @source TODO
+#' @references
+#' TODO
+#' @examples
+#' # Load data
+#' data("jdf")
+#'
+#' # TODO
+"jdf"
+
+
+#' @title Small RNA dataset
+#'
+#' @description TODO
+#'
+#' @docType data
+#' @format A data frame with TODO rows and TODO variables:
+#' \describe{
+#'   \item{TODO}{TODO}
+#'   \item{TODO}{TODO}
+#' }
+#' @details
+#' TODO
+#' @source TODO
+#' @references
+#' TODO
+#' @examples
+#' # Load data
+#' data("smallrna")
+#'
+#' # TODO
+"smallrna"
